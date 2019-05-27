fa6f69c9488780df75b0da16a08b2ba90e4ba0b9
jianghong <jianghong2@xiaoniu66.com>
2019-05-27 16:57:48 +0800

    完成单项链表

--- 单向链表/单向链表/main.swift
+++ 单向链表/单向链表/main.swift
@@ -8,5 +8,26 @@
 
 import Foundation
 
-print("Hello, World!")
+var list: LinkedList<Int> = LinkedList()
 
+list.add(1);
+list.add(3);
+list.add(5);
+list.add(2);
+list.add(8);
+
+print("删除了\(String(describing: list.remove(1)))+size:\(list.size)")
+
+print("5的下标：\(list.indexOf(5))")
+
+list.add(0, 10)
+print("重写了\(String(describing: list.set(4, 33)))+size:\(list.size)")
+
+list.remove(0)
+list.remove(list.size-1)
+
+for index in 0..<list.size {
+    print(list.get(index) as Any);
+}
+
+print("是否包含5：\(list.contains(8))")
