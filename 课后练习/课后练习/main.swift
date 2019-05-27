b6a1a4a5175764f3562d8cc60ead47566ea0756f
jianghong <jianghong2@xiaoniu66.com>
2019-05-27 17:53:09 +0800

    添加练习题

---
+++ 课后练习/课后练习/main.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  课后练习
+//
+//  Created by XNIOS on 2019/5/27.
+//  Copyright © 2019 dylan. All rights reserved.
+
+//  206. 反转链表
+//  https://leetcode-cn.com/problems/reverse-linked-list/
+
+import Foundation
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+
+class Solution {
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        return nil;
+    }
+}
