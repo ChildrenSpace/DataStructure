7f87ba8039346ce48e555262869ba33e082c415f
jianghong <jianghong2@xiaoniu66.com>
2019-05-29 10:39:48 +0800

    添加单向循环链表

---
+++ 单向循环链表/单向循环链表/LinkedList.swift
@@ -0,0 +1,140 @@
+//
+//  LinkedList.swift
+//  单向链表
+//
+//  Created by XNIOS on 2019/5/27.
+//  Copyright © 2019 Dylan. All rights reserved.
+//
+
+import Foundation
+
+let ELEMENT_NOT_FOUND: Int = -1;
+
+public class Node<T: Comparable> {
+    var element: T?
+    var next: Node<T>?
+    
+    init(_ element: T, _ next: Node<T>?) {
+        self.element = element;
+        self.next = next;
+    }
+}
+
+class LinkedList<T: Comparable> {
+    var size: Int = 0
+    var first: Node<T>?
+    
+    /// 元素的个数
+    public func sizeOfElement() -> Int {
+        return size;
+    }
+    
+    /// 清除所有元素
+    public func clear() -> Void {
+        size = 0;
+        first = nil;
+    }
+    
+    /// 是否为空
+    public func isEmpty() -> Bool {
+        return size == 0
+    }
+    
+    /// 是否包含某个元素
+    public func contains(_ element: T) -> Bool {
+        return self.indexOf(element) != ELEMENT_NOT_FOUND
+    }
+    
+    /// 添加元素到尾部
+    public func add(_ element: T) -> Void {
+        self.add(size, element)
+    }
+    
+    /// 获取index位置的元素
+    public func get(_ index: Int) -> T? {
+        return node(index)?.element
+    }
+    
+    /// 设置index位置的元素
+    public func set(_ index: Int, _ element: T) -> T? {
+        let oldNode = node(index)
+        let oldElement = oldNode?.element
+        oldNode?.element = element
+        return oldElement;
+    }
+    
+    /// 在index位置插入一个元素
+    public func add(_ index: Int, _ element: T) -> Void {
+        // 检查index的合法性
+        if index < 0 || index > size {
+            fatalError("Index:\(index), Size:\(size)")
+        }
+        
+        if index == 0 {
+            let newNode = Node(element, first)
+            let lastNode = (size == 0) ? newNode : node(size - 1)
+            lastNode?.next = newNode
+            first = newNode
+        } else {
+            let preNode = node(index - 1)
+            let newNode = Node(element, preNode?.next)
+            preNode?.next = newNode
+        }
+        
+        size += 1
+    }
+    
+    /// 删除index位置的元素
+    public func remove(_ index: Int) -> T? {
+        // 检查index的合法性
+        if index < 0 || index >= size {
+            fatalError("Index:\(index), Size:\(size)")
+        }
+        
+        var delElement: T?
+        if index == 0 {
+            if size == 1 {
+                first = nil
+            } else {
+                let lastNode = node(size - 1)
+                lastNode?.next = first?.next
+                first = first?.next
+            }
+        } else {
+            let preNode = node(index-1)
+            delElement = preNode?.next?.element
+            preNode?.next = preNode?.next?.next
+        }
+        
+        size -= 1
+        return delElement;
+    }
+    
+    /// 查看元素的索引
+    public func indexOf(_ element: T?) -> Int {
+        var currentNode: Node<T>? = first
+        for index in 0..<size {
+            if element == currentNode?.element {
+                return index
+            }
+            currentNode = currentNode?.next
+        }
+        
+        return ELEMENT_NOT_FOUND
+    }
+    
+    /// 获取index位置的元素
+    private func node(_ index: Int) -> Node<T>? {
+        // 检查index的合法性
+        if index < 0 || index >= size {
+            fatalError("Index:\(index), Size:\(size)")
+        }
+        
+        var node = self.first;
+        for _ in 0..<index {
+            node = node?.next;
+        }
+        
+        return node;
+    }
+}
